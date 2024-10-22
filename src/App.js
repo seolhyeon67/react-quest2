ae5124757d96280559e1ac28cfd9af3f63b4b43f
seolhyeon67 <seolhyeon67@gmail.com>
2024-10-22 09:44:33 +0900

    react about q1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Hdr from './Header1.js';
 import Mp from './Main_picture.js';
 import IconT from './Icon_title.js';
 import Iconbox from './Iconbox.js';
 import Chefs from './Chefs.js';
+import About from './About.js'
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/About' element={<About />}/>
+        </Routes>
+      </BrowserRouter>
       <Hdr/>
       <Mp/>
       <IconT/>
--- src/Header1.js
+++ src/Header1.js
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
 import "./main.css";
-let hd=['How it works','Gifts','About',<img src="./logo.png"/>,'Login','Members','Contracts'];
+
+let hd=['How it works','Gifts',<Link to='/About'>About</Link>,<img src="./logo.png"/>,'Login','Members','Contracts']
 let a=[]
 for (let i=0;i<7;i++) a.push(<li>{hd[i]}</li>)
 function Hd () {
@@ -7,7 +9,7 @@ function Hd () {
         <div className="header">
             <ul>
                 {a}
-            </ul>
+            </ul>        
         </div>
     )
 };
